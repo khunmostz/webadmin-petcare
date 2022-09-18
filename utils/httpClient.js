4f54e7773c076c09bd54d94e212a932b16b00f62
khunmostz24 <khunmostz24@gmail.com>
2022-09-18 16:00:03 +0700

    ??

--- utils/httpClient.js
+++ utils/httpClient.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL_API,
+  baseURL:
+    "http://apicare-env.eba-em9ynm3z.ap-southeast-1.elasticbeanstalk.com/api/v1/petcare",
 });
 export default httpClient;
